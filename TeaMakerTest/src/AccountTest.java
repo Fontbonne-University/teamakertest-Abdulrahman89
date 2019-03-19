235cdea467ea157dfb6575340f27281d5efc5000
albis <albis@DESKTOP-756268P.attlocal.net>
2019-03-19 13:55:20 -0500

    Completed activities 1 to 3
    Signed-off-by: albis <albis@DESKTOP-756268P.attlocal.net>

--- TeaMakerTest/src/AccountTest.java
+++ TeaMakerTest/src/AccountTest.java
@@ -44,7 +44,46 @@ public class AccountTest {
       Date start= (Date) formatter.parse(str_start);
       Date end= (Date) formatter.parse(str_end);
 
-      assertEquals(230, anAccount.getSalesBetween(start, end));
+      assertEquals(230, anAccount.getSalesBetween(new DateRange(start, end)));
+   }
+   
+   @Test
+   public void testgetSalesBetween2() throws ParseException
+   {
+	   DateFormat formatter = new SimpleDateFormat("dd-MMM-yy");
+	   String str_start = "1-Jan-03";
+	   String str_end = "16-Jan-03";
+	   
+	   Date start = (Date) formatter.parse(str_start);
+	   Date end = (Date) formatter.parse(str_end);
+	   
+	   assertEquals(100, anAccount.getSalesBetween(new DateRange(start, end)));
+   }
+   
+   @Test
+   public void testinValidRAngeInclusive() throws ParseException
+   {
+	   DateFormat formatter = new SimpleDateFormat("dd-MMM-yy");
+	   String str_start = "16-Jan-03";
+	   String str_end = "1-Jan-03";
+	   
+	   Date start = (Date) formatter.parse(str_start);
+	   Date end = (Date) formatter.parse(str_end);
+	   
+	   assertEquals(100, anAccount.getSalesBetween(new DateRange(start, end)));
+   }
+   
+   @Test
+   public void testinValidRAngeExclusive() throws ParseException
+   {
+	   DateFormat formatter = new SimpleDateFormat("dd-MMM-yy");
+	   String str_start = "17-Jan-03";
+	   String str_end = "1-Jan-03";
+	   
+	   Date start = (Date) formatter.parse(str_start);
+	   Date end = (Date) formatter.parse(str_end);
+	   
+	   assertEquals(0, anAccount.getSalesBetween(new DateRange(start, end)));
    }
 	
 
